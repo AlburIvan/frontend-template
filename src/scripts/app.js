e606db22c20feeb9caf3f862166935a633f2bf5d
AlburIvan <albur.ivan@outlook.com>
2019-06-10 00:13:35 -0400

    Add basic template structure

---
+++ src/scripts/app.js
@@ -0,0 +1,23 @@
+// import { confetti } from 'dom-confetti';
+console.log('Welcome to my template, to begin editing, go to `src` folder and start hacking away!')
+
+
+let confettiCannon = document.querySelector('.confetti-cannon');
+
+
+
+const confettiOpts = {
+  angle: 90,
+  spread: "54",
+  startVelocity: "34",
+  elementCount: 50,
+  dragFriction: 0.1,
+  duration: 3000,
+  stagger: 0,
+  width: "11px",
+  height: "11px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
+
+// confetti(confettiCannon, confettiOpts);
